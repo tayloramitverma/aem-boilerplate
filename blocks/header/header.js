1b08875dd4cc1df4af0eeac412c22d3f74805124
Amit V <beingamitverma@gmail.com>
2026-08-13 22:27:46 +0530

    Fix button container class name in header decoration

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -134,7 +134,7 @@ export default async function decorate(block) {
   const brandLink = navBrand.querySelector('.button');
   if (brandLink) {
     brandLink.className = '';
-    brandLink.closest('.button-container').className = '';
+    brandLink.closest('.button-wrapper').className = '';
   }
 
   const navSections = nav.querySelector('.nav-sections');
